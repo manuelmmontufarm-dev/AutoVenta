9c085cbf3ce6e8ef0a773a1f80303d5dd5a1fc19
manuelmmontufarm-dev <manuelmmontufarm@gmail.com>
2026-07-20 09:20:21 -0500

    feat(hub): add Showroom GP demo

--- hub/src/main.tsx
+++ hub/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./design/tokens.css";
 // Temas alternativos (estilos 03/04/05): inertes salvo que <html data-theme> los active
 import "./design/themes/showroom.css";
+import "./design/themes/showroom-gp.css";
 import "./design/themes/racing.css";
 import "./design/themes/neobrutalista.css";
 
